a64599fca373ccda3ea956320dd572a811b889ce
CanK <kattwinkel@weiglewilczek.com>
2015-05-07 10:42:48 +0200

    minor fix

--- src/app/app.js
+++ src/app/app.js
@@ -18,8 +18,8 @@ angular.module('w11kcal.app', [
 ]);
 
 angular.module('w11kcal.app').constant('AppKey', '41485cd87d154168dd6db06cdd3ffd69');
-angular.module('w11kcal.app').constant('baseUrl', 'http://localhost:9000');
-//angular.module('w11kcal.app').constant('baseUrl', 'http://trello-calendar.w11k.de');
+//angular.module('w11kcal.app').constant('baseUrl', 'http://localhost:9000');
+angular.module('w11kcal.app').constant('baseUrl', 'http://trello-calendar.w11k.de');
 
 
 

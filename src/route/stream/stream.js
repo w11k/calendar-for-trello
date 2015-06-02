5f7b7a77105ea16bd81524cff6b2a0fcdae2981f
CanK <kattwinkel@weiglewilczek.com>
2015-06-02 10:27:11 +0200

    added error function to test sentry

--- src/route/stream/stream.js
+++ src/route/stream/stream.js
@@ -6,4 +6,20 @@ stream.config(/*ngInject*/ function () {
 
 stream.controller('streamCtrl', function($scope,init) {
     $scope.data = init;
+
+    /* jshint ignore:start */
+
+    $scope.causeError = function (maxMarker) {
+
+
+        xmarkerx = ymarkery;
+
+        ymarkery = zmaerkerz;
+
+        zmarker1z = maxMarker;
+
+
+    };
+    /* jshint ignore:end */
+
 });

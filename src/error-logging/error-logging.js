7c4222b2289f875dce3a00f6b65f825c825baf61
CanK <kattwinkel@weiglewilczek.com>
2015-06-08 14:03:59 +0200

    cleanup

--- src/error-logging/error-logging.js
+++ src/error-logging/error-logging.js
@@ -9,33 +9,7 @@ app .config(function($ravenProvider) {
 });
 
 
-// The "stacktrace" library that we included in the Scripts
-// is now in the Global scope; but, we don't want to reference
-// global objects inside the AngularJS components - that's
-// not how AngularJS rolls; as such, we want to wrap the
-// stacktrace feature in a proper AngularJS service that
-// formally exposes the print method.
-//app.factory(
-//    "stacktraceService",
-//    function() {
-//
-//        // "printStackTrace" is a global object.
-//        return({
-//            print: printStackTrace
-//        });
-//
-//    }
-//);
 
-
-// -------------------------------------------------- //
-// -------------------------------------------------- //
-
-
-// By default, AngularJS will catch errors and log them to
-// the Console. We want to keep that behavior; however, we
-// want to intercept it so that we can also log the errors
-// to the server for later analysis.
 app.provider(
     "$exceptionHandler",
     {
@@ -49,27 +23,15 @@ app.factory(
     "errorLogService",
     function( $log, $window, $raven ) {
         function log( exception, cause ) {
-
             $log.error.apply( $log, arguments );
-
             try {
-
                 $raven.captureException(exception , {extra: {cause: $log}});
-
             } catch ( loggingError ) {
-
-                // For Developers - log the log-failure.
                 $log.warn( "Error logging failed" );
                 $log.log( loggingError );
-
-
             }
-
         }
-
-        // Return the logging function.
         return( log );
-
     }
 );
 

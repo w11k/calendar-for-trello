bbbe06b95cdf43b0f8278fb33bfd6e72068b03d6
CanK <kattwinkel@weiglewilczek.com>
2015-07-02 13:48:55 +0200

    xhr error logging

--- src/error-logging/error-logging.js
+++ src/error-logging/error-logging.js
@@ -1,11 +1,23 @@
 'use strict';
 
-var app = angular.module( 'trelloCal.errorLogging', ['ngRaven'] );
-app.config(function($provide) {
+var app = angular.module( 'trelloCal.errorLogging', ['ngRaven']);
+app.config(function($provide,$httpProvider) {
+    // Capture Angular Errors
     $provide.decorator('$exceptionHandler', ['$delegate', '$raven', function ($delegate, $raven) {
         return function (exception, cause) {
             $delegate(exception, cause);
             $raven.captureException(exception);
+        };
+    }]);
+
+
+    // in addition, capture XHR Errors
+    $httpProvider.interceptors.push(['$q', '$raven', function($q,$raven) {
+        return {
+            responseError: function(rejection) {
+                $raven.captureException(rejection);
+                return $q.reject(rejection);
+            }
         };
     }]);
 });

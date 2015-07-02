0d6a31480646db799120200d539887cbb8a9f7a5
CanK <kattwinkel@weiglewilczek.com>
2015-07-02 13:19:39 +0200

    changed errorlogging

--- src/error-logging/error-logging.js
+++ src/error-logging/error-logging.js
@@ -1,39 +1,11 @@
-/* jshint ignore:start */
+'use strict';
 
-// Create an application module for our demo.
-var app = angular.module( "trelloCal.errorLogging", ['ngRaven'] );
-
-app .config(function($ravenProvider) {
-    // There is a development flag to log errors rather than sending it to Sentry
-    $ravenProvider.development(false);
+var app = angular.module( 'trelloCal.errorLogging', ['ngRaven'] );
+app.config(function($provide) {
+    $provide.decorator('$exceptionHandler', ['$delegate', '$raven', function ($delegate, $raven) {
+        return function (exception, cause) {
+            $delegate(exception, cause);
+            $raven.captureException(exception);
+        };
+    }]);
 });
-
-
-
-app.provider(
-    "$exceptionHandler",
-    {
-        $get: function(errorLogService) {
-            return(errorLogService);
-        }
-    }
-);
-
-app.factory(
-    "errorLogService",
-    function($log,$window,$raven) {
-        function log(exception,cause) {
-            $log.error.apply($log, arguments);
-            try {
-                $raven.captureException(exception);
-            } catch (loggingError) {
-                $log.warn( "Error logging failed" );
-                $log.log(loggingError);
-            }
-        }
-        return(log);
-    }
-);
-
-
-/* jshint ignore:end */

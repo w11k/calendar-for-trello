bf1465fcfc0fe69bf201e75063d355790a246da5
Can Kattw <kattwinkel@w11k.de>
2016-12-13 14:00:21 +0100

    re #88 re #89 improve cards design

--- src/app/calendar/calendar-card/calendar-card.component.ts
+++ src/app/calendar/calendar-card/calendar-card.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, Input} from '@angular/core';
+import {Component, OnInit, Input, HostBinding} from '@angular/core';
 import {Card} from "../../models/card";
 import {select} from "ng2-redux";
 import {Observable} from "rxjs";
@@ -13,10 +13,10 @@ import * as _ from "lodash"
 export class CalendarCardComponent implements OnInit {
 
   @Input() public card: Card;
-  color: string;
   @select(state => state.settings.boardColorPrefs) public boardColorPrefs$: Observable<Object>;
   @select(state => state.boards) public boards$: Observable<Object>;
 
+  @HostBinding('style.border-left-color') borderLeft;
   constructor() {
   }
 
@@ -26,7 +26,7 @@ export class CalendarCardComponent implements OnInit {
       .subscribe(x => {
         let boardColorPrefs = x[0];
         let board = _.find(x[1], (board: Board) => board.id === this.card.idBoard);
-        this.color = boardColorPrefs[this.card.idBoard] || (board ? board.prefs.backgroundColor : null);
+        this.borderLeft = boardColorPrefs[this.card.idBoard] || (board ? board.prefs.backgroundColor : null);
       })
   }
 }

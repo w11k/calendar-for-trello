13734e35de82600ca8ef7600467fee405d368a96
Can Kattw <kattwinkel@w11k.de>
2017-01-14 23:08:57 +0100

    re #112: show all cards per default

--- src/app/models/settings.ts
+++ src/app/models/settings.ts
@@ -1,6 +1,6 @@
 import {CalendarType} from "../redux/actions/settings-actions";
 export class Settings {
-  public observerMode: boolean;
+  public observerMode: boolean = true;
   public boardColorPrefs: Object = {};
   public boardVisibilityPrefs: Object = {};
   public language: any;

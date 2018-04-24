b93e6fe4996330a6466e1e1d70241024981bd712
Jan Blankenhorn <blankenhorn@w11k.de>
2018-04-24 16:40:06 +0200

    test #158. moved drop zone to floating element

---
+++ src/app/services/drop-zone.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {Subject} from 'rxjs/Subject';
+
+@Injectable()
+export class DropZoneService {
+
+  public channel: Subject<boolean> = new Subject();
+
+  constructor() { }
+
+  public getUpdates() {
+    return this.channel.asObservable();
+  }
+
+  public dragStart() {
+    this.channel.next(true);
+  }
+
+  public dragStop() {
+    this.channel.next(false);
+  }
+}

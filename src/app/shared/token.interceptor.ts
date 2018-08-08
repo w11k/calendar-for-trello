bcb5f221e01eab654f5372b7846f0d1ebd0692ef
Can Kattwinkel <kattwinkel@w11k.de>
2018-08-08 15:34:54 +0200

    fix(interceptor): respect existing params

--- src/app/shared/token.interceptor.ts
+++ src/app/shared/token.interceptor.ts
@@ -13,7 +13,14 @@ export class TokenInterceptor implements HttpInterceptor {
   }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const params = new HttpParams().append('token', this.trelloAuthService.getToken()).append('key', config.apiKey);
+
+    let params = new HttpParams().append('token', this.trelloAuthService.getToken()).append('key', config.apiKey);
+
+    // Copy old Params:
+    request.params.keys().forEach(key => {
+      params = params.set(key, request.params.get(key));
+    });
+
 
     request = request.clone({
       params: params,

ac15aeab86c31644c357561625d6ea00d8ce1f2c
Oliver Schwenk <schwenk@w11k.de>
2019-11-05 16:20:40 +0100

    update npm run start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-
 import {AppComponent} from './app.component';
 import {routing} from './routes';
 import {CalendarDaysService} from './services/calendar-days.service';
--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,9 @@ import {hmrBootstrap} from './hmr';
 import {enableProdMode} from '@angular/core';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
-import {AppModule} from './app/app.module';
 import {environment} from './environments/environment';
 import 'hammerjs';
+import {AppModule} from './app';
 
 const project = require('../package.json');
 
